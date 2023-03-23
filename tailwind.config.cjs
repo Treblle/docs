b18d2986d80661c79d2d88989d38c4a57bb29767
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-03-23 16:31:17 +0100

    chore: setup Treblle color in Tailwind CSS config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'treblle-blue': {
+          DEFAULT: '#0047BB',
+          50: '#74A9FF',
+          100: '#5F9CFF',
+          200: '#3683FF',
+          300: '#0E69FF',
+          400: '#0056E4',
+          500: '#0047BB',
+          600: '#003283',
+          700: '#001C4B',
+          800: '#000713',
+          900: '#000000',
+        },
+      },
+    },
   },
   plugins: [],
 }
