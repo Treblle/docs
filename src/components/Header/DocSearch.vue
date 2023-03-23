58460e502abcc476c9de7844d8098f5827f0b963
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-03-23 16:32:54 +0100

    feat(wip): first implementation of custom search in docs

---
+++ src/components/Header/DocSearch.vue
@@ -0,0 +1,132 @@
+<script setup>
+import { ref, watch } from 'vue'
+import { useMagicKeys, useFetch, useDebounceFn } from '@vueuse/core'
+import { ALGOLIA } from '../../config'
+const keys = useMagicKeys()
+const cmdK = keys['Meta+K']
+const escapeKey = keys['escape']
+const isOpen = ref(false)
+const query = ref('')
+const searchUrl = ref(
+  `https://${ALGOLIA.appId}-dsn.algolia.net/1/indexes/${ALGOLIA.indexName}/query`
+)
+const searchHeaders = {
+  'X-Algolia-API-Key': ALGOLIA.apiKey,
+  'X-Algolia-Application-Id': ALGOLIA.appId,
+}
+
+const searchResults = ref(null)
+
+watch(cmdK, (v) => {
+  if (v) toggleSearchModal()
+})
+
+watch(escapeKey, (v) => {
+  if (v) closeSearchModal()
+})
+
+const debouncedFn = useDebounceFn(async () => {
+  const { data } = await useFetch(searchUrl.value, { headers: searchHeaders })
+    .post({ params: `query=${query.value}` })
+    .json()
+  searchResults.value = data.value
+}, 1000)
+
+watch(query, (v) => {
+  if (v) debouncedFn()
+})
+
+const openSearchModal = () => {
+  isOpen.value = true
+}
+const closeSearchModal = () => {
+  searchResults.value = null
+  query.value = ''
+  isOpen.value = false
+}
+const toggleSearchModal = () => {
+  isOpen.value = !isOpen.value
+}
+</script>
+<template>
+  <button @click="openSearchModal">Search</button>
+  <Transition name="modal">
+    <section
+      v-if="isOpen"
+      class="modal-mask fixed inset-0 z-50 flex h-screen w-screen items-start justify-center"
+    >
+      <div
+        class="search-modal mt-28 w-full self-start rounded-sm px-6 py-8 sm:w-5/6 lg:w-2/5"
+      >
+        <div class="relative flex">
+          <div class="flex items-start">
+            <h3 class="flex space-x-2 text-3xl">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-search h-8 w-8 text-treblle-blue"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+                />
+              </svg>
+              <span>Search documentation</span>
+            </h3>
+            <button
+              class="absolute -right-6 -top-8 text-lg"
+              @click="closeSearchModal"
+            >
+              &#x2717;
+            </button>
+          </div>
+        </div>
+        <div>
+          <input
+            placeholder="Start typing..."
+            v-model="query"
+            autofocus="true"
+            class="w-full rounded-sm border bg-transparent px-4 py-3 focus:outline-gray-100/80"
+          />
+
+          <ul class="max-h-96 overflow-auto rounded-b-sm bg-gray-100">
+            <li
+              v-for="result in searchResults?.hits"
+              class="px-4 py-2 hover:bg-gray-200"
+            >
+              <a :href="result.url" class="hover:no-underline">
+                <p class="font-bold">{{ result.title }}</p>
+                <p class="truncate">{{ result.description }}</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </Transition>
+</template>
+<style>
+.modal-mask {
+  background-color: var(--modal-mask-bg);
+}
+.search-modal {
+  background-color: var(--theme-bg);
+  box-shadow: 0px 0px 9px -3px hsla(var(--color-gray-5), 0.3);
+  border-color: var(--theme-bg);
+}
+.modal-enter-from {
+  opacity: 0;
+}
+
+.modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-from .modal-container,
+.modal-leave-to .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
